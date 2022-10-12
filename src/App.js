fbb415fbf438cb97d7f9efaea99f361aa5acd10c
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-10-12 15:16:05 +0200

    Add a title and a favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './Component/Home';
 import Region from './Component/Region';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'covid states';
+  });
   return (
     <div className="App">
       <Routes>
